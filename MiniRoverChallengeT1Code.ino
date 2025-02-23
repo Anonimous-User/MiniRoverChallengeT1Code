d7472b6799f8a97cf251e97afc2189fcf85f8b39
anonimous-user <harrisbond1234@gmail.com>
2025-02-23 16:57:49 -0500

    updates and file management

--- MiniRoverChallengeT1Code.ino
+++ MiniRoverChallengeT1Code.ino
@@ -8,21 +8,21 @@ Servo buttonPress;
 #define bPress 9             //button presser
 #define aBase 10             //arm at base
 #define aMid 11              //arm in middle
-// #define SensorL 45            //ir sensor Left
-// #define SensorM 43            //ir sensor Middle
-// #define SensorR 41            //ir sensor Right
+#define SensorL 4            //ir sensor Left
+#define SensorM 5            //ir sensor Middle
+#define SensorR 6            //ir sensor Right
 #define LeftFrontforward 7   //front left wheel forward
 #define LeftFrontreverse 8   //front left wheel reverse
 #define RightFrontforward 12  //front right wheel forward
 #define RightFrontreverse 13  //front right wheel reverse
 #define LFCtrl A2            //left front control
 #define RFCtrl A3            //right front control
-// #define LeftRearforward 42   //rear left wheel forward
-// #define LeftRearreverse 43    //rear left wheel reverse
-// #define RightRearforward 44  //rear right wheel forward
-// #define RightRearreverse 45  //rear right wheel reverse
-// #define LRCtrl A4            //left rear control
-// #define RRCtrl A5            //right rear control
+#define LeftRearforward 3   //rear left wheel forward
+#define LeftRearreverse 2    //rear left wheel reverse
+#define RightRearforward A0  //rear right wheel forward
+#define RightRearreverse A1  //rear right wheel reverse
+#define LRCtrl A4            //left rear control
+#define RRCtrl A5            //right rear control
 // #define CUSTOM_SETTINGS
 // #define INCLUDE_GAMEPAD_MODULE
 
@@ -33,6 +33,7 @@ bool onLine = false;
 bool atEnd = false;
 int aBasePos = 0;
 int aMidPos = 0;
+int bPressPos = 0;
 
 
 /////////////////
@@ -60,37 +61,40 @@ void TurnFrontRight(int val) {
     analogWrite(RFCtrl, val);
   }
 }
-// void TurnRearLeft(int val) {
-//   if (val < 0) {
-//     digitalWrite(LeftRearforward, LOW);
-//     digitalWrite(LeftRearreverse, HIGH);
-//     analogWrite(LRCtrl, val * -1);
-//   } else {
-//     digitalWrite(LeftRearforward, HIGH);
-//     digitalWrite(LeftRearreverse, LOW);
-//     analogWrite(LRCtrl, val);
-//   }
-// }
-// void TurnRearRight(int val) {
-//   if (val < 0) {
-//     digitalWrite(RightRearforward, LOW);
-//     digitalWrite(RightRearreverse, HIGH);
-//     analogWrite(RRCtrl, val * -1);
-//   } else {
-//     digitalWrite(RightRearforward, HIGH);
-//     digitalWrite(RightRearreverse, LOW);
-//     analogWrite(RRCtrl, val);
-//   }
-// }
+void TurnRearLeft(int val) {
+  if (val < 0) {
+    digitalWrite(LeftRearforward, LOW);
+    digitalWrite(LeftRearreverse, HIGH);
+    analogWrite(LRCtrl, val * -1);
+  } else {
+    digitalWrite(LeftRearforward, HIGH);
+    digitalWrite(LeftRearreverse, LOW);
+    analogWrite(LRCtrl, val);
+  }
+}
+void TurnRearRight(int val) {
+  if (val < 0) {
+    digitalWrite(RightRearforward, LOW);
+    digitalWrite(RightRearreverse, HIGH);
+    analogWrite(RRCtrl, val * -1);
+  } else {
+    digitalWrite(RightRearforward, HIGH);
+    digitalWrite(RightRearreverse, LOW);
+    analogWrite(RRCtrl, val);
+  }
+}
 void Drive(int FrontLeft, int FrontRight, int RearLeft, int RearRight){
     TurnFrontLeft(FrontLeft);
     TurnFrontRight(FrontRight);
-    // TurnRearLeft(RearLeft);
-    // TurnRearRight(RearRight);
+    TurnRearLeft(RearLeft);
+    TurnRearRight(RearRight);
 }
 void MoveArm(){
   armBase.write(aBasePos);
   armMiddle.write(aMidPos);
+}
+void PushButton(){
+  buttonPress.write(bPressPos);
 }
 
 
@@ -197,21 +201,21 @@ void autoMode(){
 void setup() {
   armBase.attach(aBase);
   armMiddle.attach(aMid);
-  // pinMode(SensorL, INPUT);
-  // pinMode(SensorM, INPUT);
-  // pinMode(SensorR, INPUT);
+  pinMode(SensorL, INPUT);
+  pinMode(SensorM, INPUT);
+  pinMode(SensorR, INPUT);
   pinMode(LeftFrontforward, OUTPUT);
   pinMode(LeftFrontreverse, OUTPUT);
   pinMode(RightFrontforward, OUTPUT);
   pinMode(RightFrontreverse, OUTPUT);
-  // pinMode(LeftRearforward, OUTPUT);
-  // pinMode(LeftRearreverse, OUTPUT);
-  // pinMode(RightRearforward, OUTPUT);
-  // pinMode(RightRearreverse, OUTPUT);
+  pinMode(LeftRearforward, OUTPUT);
+  pinMode(LeftRearreverse, OUTPUT);
+  pinMode(RightRearforward, OUTPUT);
+  pinMode(RightRearreverse, OUTPUT);
   pinMode(LFCtrl, OUTPUT);
   pinMode(RFCtrl, OUTPUT);
-  // pinMode(LRCtrl, OUTPUT);
-  // pinMode(RRCtrl, OUTPUT);
+  pinMode(LRCtrl, OUTPUT);
+  pinMode(RRCtrl, OUTPUT);
   Serial.begin(9600);
   // Dabble.begin(9600);
   delay(1000);
@@ -219,37 +223,51 @@ void setup() {
   //reset all positions
   TurnFrontLeft(0);
   TurnFrontRight(0);
-  // TurnRearLeft(0);
-  // TurnRearRight(0);
+  TurnRearLeft(0);
+  TurnRearRight(0);
   armBase.write(0);
   armMiddle.write(0);
+  buttonPress.write(0);
 }
 
 void loop() {
 
-  Drive(255, 255, 255, 255);
-  delay(1000);
-  Drive(0, 0, 0, 0);
-  for(int i=0; i<90; i++){
-    aBasePos=i;
-    MoveArm();
-    delay(15);
-  }
-  for(int i=0; i<80; i++){
-    aMidPos=i;
-    MoveArm();
-    delay(15);
-  }
-  for(int i=80; i>0; i--){
-    aMidPos=i;
-    MoveArm();
-    delay(15);
-  }
-  for(int i=90; i>0; i--){
-    aBasePos=i;
-    MoveArm();
-    delay(15);
-  }
+    for(int i=0; i<90; i++){
+      bPressPos=i;
+      PushButton();
+      delay(15);
+    }
+    for(int i=90; i>0; i--){
+      bPressPos=i;
+      PushButton();
+      delay(15);
+    }
+
+  // Drive(255, 255, 255, 255);
+  // delay(1000);
+  // Drive(-255, 255,0,0);
+  // delay(1000);
+  // Drive(0, 0, 0, 0);
+  // for(int i=0; i<90; i++){
+  //   aBasePos=i;
+  //   MoveArm();
+  //   delay(15);
+  // }
+  // for(int i=0; i<80; i++){
+  //   aMidPos=i;
+  //   MoveArm();
+  //   delay(15);
+  // }
+  // for(int i=80; i>0; i--){
+  //   aMidPos=i;
+  //   MoveArm();
+  //   delay(15);
+  // }
+  // for(int i=90; i>0; i--){
+  //   aBasePos=i;
+  //   MoveArm();
+  //   delay(15);
+  // }
 
   //////////////
   //Autonomous//
